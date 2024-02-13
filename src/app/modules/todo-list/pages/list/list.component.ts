5e31ed2cd6006afe520eea13e4e2e09709dd1576
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2024-02-13 18:48:57 -0300

    alert

--- src/app/modules/todo-list/pages/list/list.component.ts
+++ src/app/modules/todo-list/pages/list/list.component.ts
@@ -3,6 +3,7 @@ import { InputAddItemComponent } from '../../components/input-add-item/input-add
 import { TodoListItem } from '../../interface/todoListItem.interface';
 import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 import { LocalStorageEnum } from '../../enum/localStorage.enum';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -51,7 +52,7 @@ export class ListComponent {
 
   public updateItemCheckbox(newItem: { checked: boolean, id: string }) {
     this.#setListItems.update((oldValue: TodoListItem[]) => {
-      oldValue.filter( res => {
+      oldValue.filter(res => {
         if (res.id === newItem.id) {
           res.checked = newItem.checked;
 
@@ -67,34 +68,69 @@ export class ListComponent {
     return this.#updateLocalStorage();
   }
 
-public updateItemText(newItem: { value: string, id: string }) {
-  this.#setListItems.update((oldValue: TodoListItem[]) => {
-    oldValue.filter( res => {
-      if (res.id === newItem.id) {
-        res.value = newItem.value;
+  public updateItemText(newItem: { value: string, id: string }) {
+    this.#setListItems.update((oldValue: TodoListItem[]) => {
+      oldValue.filter(res => {
+        if (res.id === newItem.id) {
+          res.value = newItem.value;
+
+          return res
+        }
 
         return res
-      }
+      })
 
-      return res
-    })
-
-    return oldValue
-  })
-
-  return this.#updateLocalStorage();
-}
-
-  public deleteItem(id: string) {
-    this.#setListItems.update((oldValue: TodoListItem[]) => {
-      return oldValue.filter( (res) => res.id !== id )
+      return oldValue
     })
 
     return this.#updateLocalStorage();
   }
 
+  public deleteItem(id: string) {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#486c9e",
+      cancelButtonColor: "#a92a39",
+      confirmButtonText: "Yes, delete task!",
+      cancelButtonText: "I'd rather keep it",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue: TodoListItem[]) => {
+          return oldValue.filter((res) => res.id !== id)
+        })
+
+        return this.#updateLocalStorage();
+      }
+    });
+
+
+  }
+
   public deleteAllItems() {
-    localStorage.removeItem(LocalStorageEnum.MY_LIST);
-    return this.#setListItems.set(this.#parseItems());
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#486c9e",
+      cancelButtonColor: "#a92a39",
+      confirmButtonText: "Yes, delete all!",
+      cancelButtonText: "I'd rather cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your tasks have been deleted.",
+          icon: "success",
+          confirmButtonColor: "#486c9e"
+        });
+
+        localStorage.removeItem(LocalStorageEnum.MY_LIST);
+        return this.#setListItems.set(this.#parseItems());
+      }
+    });
   }
 }
